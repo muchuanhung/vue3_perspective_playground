0955374a491d18898c416da7f1a9bdf4c163f7a0
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2022-05-17 18:08:51 +0800

    deploy setting

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f https://github.com/muchuanhung/vue3_perspective_playground.git master:gh-pages
+git push -f https://github.com/muchuanhung/vue3_perspective_playground.git main:gh-pages
 
 cd -
